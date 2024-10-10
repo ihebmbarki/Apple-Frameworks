5f9f2d2e9038cf17cec79f67dafdc7ebf7558bd2
ihebmbarki <iheb.mbarki@esprit.tn>
2024-10-10 15:04:11 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Iheb Mbarki on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
